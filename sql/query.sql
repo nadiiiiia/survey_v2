e2f207d85261754887ac33c5fcecfa73d54a557d
nadiiiiia <nadia.hbr@gmail.com>
2017-12-21 12:11:52 +0100

    update BD

--- sql/query.sql
+++ sql/query.sql
@@ -16,3 +16,11 @@ ALTER TABLE `survey_questions` ADD `question_back`  INT DEFAULT 0 AFTER `questio
 -- Augmenter la taille de la réponse (5/12/2017)
 --
 ALTER TABLE `survey_answers` CHANGE  `answer_body` `answer_body` VARCHAR(1000) DEFAULT '' ;
+--
+-- Ajouter des champs dans la table ref_mail_list (21/12/2017)
+--
+ALTER TABLE `ref_mail_list` ADD `new_contact`  varchar(37) DEFAULT NULL, AFTER `Personne_contact`;
+
+ALTER TABLE `ref_mail_list` ADD `new_mail`  varchar(49) DEFAULT NULL, AFTER `Contact_mail`;
+
+ALTER TABLE `ref_mail_list` ADD `new_telephone `  varchar(14) DEFAULT NULL, AFTER `contact_téléphonique`;
